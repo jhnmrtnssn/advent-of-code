a4839b9af44d02940bd0ae5819fa3b84514f6697
Johan Martinsson <martinsson.johan27@gmail.com>
2021-12-06 13:14:56 +0100

    day6

---
+++ day6/part1.py
@@ -0,0 +1,30 @@
+# Advent of Code - Day 6
+# Part 1
+import time
+import numpy as np
+
+def parseInput():
+    for line in (open("input.txt")):
+        return list(map(np.uint8, (line.strip().split(","))))
+
+
+fish_list = parseInput()
+#fish_list = [3, 4, 3, 0, 2]
+
+start_time = time.time()
+for day in range(0, 80):
+    n_new_fish = 0
+    new_fish_list = []
+    for id, days in enumerate(fish_list):
+        if days == 0:
+            fish_list[id] = 6
+            n_new_fish += 1
+        else:
+            fish_list[id] -= 1
+
+    new_fish_list = [8] * n_new_fish
+    fish_list += new_fish_list
+    print(day)
+
+print(time.time() - start_time)
+print(len(fish_list))
---
+++ day6/part2.py
@@ -0,0 +1,27 @@
+# Advent of Code - Day 6
+# Part 1
+import time
+import numpy as np
+
+def parseInput():
+    for line in (open("input.txt")):
+        return list(map(np.uint8, (line.strip().split(","))))
+
+
+input_fish_list = parseInput()
+#input_fish_list = [3, 4, 3, 1, 2]
+fish_list = []
+for i in range(0, 9):
+    fish_list.append(input_fish_list.count(i))
+
+start_time = time.time()
+for day in range(0, 256):
+    born_fish = fish_list[0]
+    for i in range(0, len(fish_list)):
+        if i > 0:
+            fish_list[i-1] = fish_list[i]
+    fish_list[8] = born_fish
+    fish_list[6] += born_fish
+
+
+print(sum(fish_list))
