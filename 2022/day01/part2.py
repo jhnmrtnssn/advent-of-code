8dc6c3349be9e225b5e95edfaaf3b905e8492a7e
Johan Martinsson <johan.martinsson@kollmorgen.com>
2022-12-02 16:36:04 +0100

    day1

--- 2022/day01/part2.py
+++ 2022/day01/part2.py
@@ -1,5 +1,5 @@
 # Advent of Code - Day 1
-# Part 1
+# Part 2
 
 
 def parse_input(file):
@@ -9,23 +9,24 @@ def parse_input(file):
     return data
 
 
-def find_max_calories(data):
-    max_calories = 0
+def find_three_max_calories(data):
+    max_calories = [0, 0, 0]
     calories = 0
     for line in data:
         if line != "":
             calories += int(line)
         else:
-            if calories > max_calories:
-                max_calories = calories
+            if calories > min(max_calories):
+                max_calories.append(calories)
+                max_calories.remove(min(max_calories))
             calories = 0
     return max_calories
 
 
 def main():
     data = parse_input("input.txt")
-    max_calories = find_max_calories(data)
-    print(max_calories)
+    max_calories = find_three_max_calories(data)
+    print(sum(max_calories))
 
 
 if __name__ == "__main__":
