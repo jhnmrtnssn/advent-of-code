ace3c6dfeecc55a50a19a51acb0e2599598fe81e
Johan Martinsson <johan.martinsson@kollmorgen.com>
2022-12-17 02:27:30 +0100

    day16 part1

---
+++ 2022/day16/part1.py
@@ -0,0 +1,100 @@
+# Advent of Code - Day 16
+# Part 1
+
+from typing import Dict, List
+
+
+def parse_input(file):
+    data = {}
+    for line in open(file):
+        line = line.strip().split(" has flow rate=")
+        name = line[0].split(" ")[1]
+        flow = int(line[1].split(";")[0])
+        valves = line[1].split("valve")[1].split(", ")
+        if valves[0].startswith("s"):
+            valves[0] = valves[0].split("s ")[1]
+        if valves[0].startswith(" "):
+            valves[0] = valves[0].strip()
+        data[name] = {"name": name, "flow": flow, "connecting": valves}
+    return data
+
+
+class PressureOptimizer:
+    def __init__(self, data: Dict):
+        self.valves = data
+        self.flow_valves = self.get_flow_valves()
+        self.start = self.valves["AA"]
+        self.traveled: List[Dict] = []
+        self.time_left = 30
+        self.best_score = 0
+        print(self.simulate_best_score(self.start, 0, [], self.time_left))
+
+    def get_flow_valves(self):
+        flow_valves = {}
+        for name in self.valves:
+            if self.valves[name]["flow"] > 0:
+                flow_valves[name] = self.valves[name]
+        return flow_valves
+
+    def travel_and_open_valve(self, start_valve, goal_valve, time_left):
+        dist = self.get_shortest_distance(start_valve, goal_valve) + 1
+        if time_left - dist < 0:
+            return time_left, 0
+        time_left -= dist
+        return time_left, time_left * goal_valve["flow"]
+
+    def get_shortest_distance(self, start_valve, goal_valve):
+        distance = 0
+        valves_to_visit = start_valve["connecting"]
+        visited_valves = [start_valve["name"]]
+        new_valves = []
+        found_valve = False
+        while not found_valve:
+            distance += 1
+            new_valves = []
+            for valve_name in valves_to_visit:
+                if valve_name in visited_valves:
+                    continue
+                visited_valves.append(valve_name)
+                if valve_name == goal_valve["name"]:
+                    found_valve = True
+                    break
+                for connecting_valve in self.valves[valve_name]["connecting"]:
+                    if connecting_valve not in visited_valves:
+                        new_valves.append(connecting_valve)
+            valves_to_visit = new_valves
+
+        return distance
+
+    def simulate_best_score(self, start_valve, score, traveled, time_left):
+        best_score = 0
+        prev_score = score
+        for valve_name in self.flow_valves:
+            new_score = score
+            if valve_name in traveled:
+                continue
+            new_time_left, travel_score = self.travel_and_open_valve(
+                start_valve, self.valves[valve_name], time_left
+            )
+            if travel_score > 0:
+                traveled.append(valve_name)
+                new_score += self.simulate_best_score(
+                    self.valves[valve_name], travel_score, traveled, new_time_left
+                )
+                if new_score == prev_score:
+                    new_score += travel_score
+                traveled.pop(traveled.index(valve_name))
+                if new_score > best_score:
+                    best_score = new_score
+                if new_score > self.best_score:
+                    self.best_score = new_score
+        return best_score
+
+
+def main():
+    data = parse_input("input.txt")
+    PressureOptimizer(data)
+
+
+if __name__ == "__main__":
+    main()
